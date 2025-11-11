417ff8732543e5d919a3e7609f54c1c4fa4743a3
Hugo Martins <hugo.martins.nobrega@gmail.com>
2025-11-11 19:44:47 -0300

    changes made on transform_data

--- src/data_preparation/transform_data.py
+++ src/data_preparation/transform_data.py
@@ -4,14 +4,16 @@ from src.data_preparation.clean_data import clean_data
 
 
 def transform_data():
-
+       pd.set_option('display.max_columns', None)
        new_fighter_statistics = clean_data()
 
-       f1 = new_fighter_statistics[['Fighter 1', 'Fighter_1_KD', 'Fighter_1_STR', 'Fighter_1_TD', 'Fighter_1_SUB', 'Weight_Class']].copy()
-       f2 = new_fighter_statistics[['Fighter 2', 'Fighter_2_KD', 'Fighter_2_STR', 'Fighter_2_TD', 'Fighter_2_SUB', 'Weight_Class']].copy()
+       f1 = new_fighter_statistics[['Fighter 1', 'Fighter_1_KD', 'Fighter_1_STR', 'Fighter_1_TD', 'Fighter_1_SUB',
+                                    'Round', 'Time(s)', 'Weight_Class']].copy()
+       f2 = new_fighter_statistics[['Fighter 2', 'Fighter_2_KD', 'Fighter_2_STR', 'Fighter_2_TD', 'Fighter_2_SUB',
+                                    'Round', 'Time(s)', 'Weight_Class']].copy()
 
-       f1.columns = ['Fighter', 'KD', 'STR', 'TD', 'SUB', 'Weight_Class']
-       f2.columns = ['Fighter', 'KD', 'STR', 'TD', 'SUB', 'Weight_Class']
+       f1.columns = ['Fighter', 'KD', 'STR', 'TD', 'SUB', 'Total_Rounds', 'Total_Time(s)', 'Weight_Class']
+       f2.columns = ['Fighter', 'KD', 'STR', 'TD', 'SUB', 'Total_Rounds', 'Total_Time(s)', 'Weight_Class']
 
        combined = pd.concat([f1, f2], ignore_index=True)
 
@@ -23,6 +25,8 @@ def transform_data():
                      'STR': 'sum',
                      'TD': 'sum',
                      'SUB': 'sum',
+                     'Total_Rounds': 'sum',
+                     'Total_Time(s)': 'sum',
                      'Weight_Class': 'first'
               })
        )
