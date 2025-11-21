4f367c02e1bd35a878ef4662da5b0213d179329f
Hugo Martins <hugo.martins.nobrega@gmail.com>
2025-11-21 11:46:05 -0300

    Incremented fighter_analysis

--- src/analysis/fighters_analysis.py
+++ src/analysis/fighters_analysis.py
@@ -4,6 +4,8 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import numpy as np
 
+#Fazer função para encontrar os melhores lutadores de cada estilo e etc
+
 def correlation_kd_matrix(df):
     plt.figure(figsize=(10, 8))
     sns.heatmap(df[['KD', 'STR', 'Total Fights', 'Total Wins']].corr(),
@@ -25,8 +27,43 @@ def correlation_sub_matrix(df):
     plt.title("Correlação entre estatísticas de luta")
     plt.show()
 
+def top_fighter_by_weight_class():
+    return(
+        fighter_summary.sort_values(["Weight Class", "Fighter"], ascending=[True, False])
+        .groupby("Weight Class")
+        .head(3)
+        .reset_index(drop=True)
+    )
+
+def top_by_weight_class(column):
+    df_subset = fighter_summary[['Fighter', 'Weight Class', column]].copy()
+    return(
+        df_subset.sort_values(["Weight Class", column], ascending=[True, False])
+        .groupby("Weight Class")
+        .head(3)
+        .reset_index(drop=True)
+    )
+
+def chance_of_kd_sub(column):
+    df_subset = fighter_summary[['Fighter', 'Total Fights', column]].copy()
+    df_subset['Prob ' + column] = ((df_subset[column]/df_subset['Total Fights']) * 100).round(2)
+    return(
+        df_subset.sort_values(column, ascending=False)
+        .head(10)
+        .reset_index(drop=True)
+    )
+
+def most_used_methods():
+    ...
+
 if __name__ == '__main__':
     fighter_summary, men_fighter_summary, women_fighter_summary = load_processed_data()
     #correlation_kd_matrix(fighter_summary)
     #correlation_td_matrix(fighter_summary)
-    correlation_sub_matrix(fighter_summary)
+    #correlation_sub_matrix(fighter_summary)
+    #print(top_fighter_by_weight_class())
+    #print(top_by_weight_class('KD'))
+    #print(top_by_weight_class('SUB'))
+    #print(top_by_weight_class('TD'))
+    #print(chance_of_kd_sub('KD'))
+    print(chance_of_kd_sub('SUB'))
